b7bc09698f6128841a01eb5025fbc7cdd504faad
Cyril Abella <cabella@eukles.com>
2022-03-21 10:23:10 +0100

    fix(QueryModifier): renaming et typing

--- src/Service/QueryModifier/UseQuery/UseQueryFromDotNotation.php
+++ src/Service/QueryModifier/UseQuery/UseQueryFromDotNotation.php
@@ -32,7 +32,7 @@ class UseQueryFromDotNotation
      */
     protected $query;
 
-    private static $counter = 0;
+    private static int $aliasesCnt = 0;
 
     /**
      * UseQueryFromDotNotation constructor.
@@ -134,7 +134,7 @@ class UseQueryFromDotNotation
                     $path = implode(self::RELATION_SEP, $this->map);
                     throw new RelationNotFoundException("Relation \"$relation\" Not Found in \"$path\"");
                 }
-                $alias = 'alias_' . self::$counter++;
+                $alias = 'alias_' . self::$aliasesCnt++;
                 $this->query = call_user_func([$this->query, $method], "`" . $alias . "_" . $relation . "`", $joinType);
             }
         }
